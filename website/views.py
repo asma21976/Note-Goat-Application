89ba0224ffc091a5d181ff8f51a1d00632ada605
ColeB <butueth@gmail.com>
2022-03-10 22:44:51 -0700

    Finished the basic functionality of the home-page. This includes clicking on different folders to see different notes and redirects to not implemented links

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+from ast import For
+from urllib import request
 from django.shortcuts import render
 from django.views.generic import (
     TemplateView, CreateView, ListView, UpdateView, DeleteView,
@@ -9,19 +11,39 @@ from .models import (
 )
 
 # Create your views here.
-
-from django.views.generic import TemplateView
-
+from django.http import HttpResponse
+from django.views.generic import TemplateView, DetailView, ListView, FormView, View
+from django.views.generic.edit import FormMixin
+from django.template import loader
 from website import models
 
 
-class HomePageView(CreateView):
-    template_name = 'homepage.html'
-    model = Folder
-    fields = ('folder_name',)
-    context_object_name = 'folders'
-    # queryset = Folder.objects.all()
-    success_url = reverse_lazy('home')
+class HomePageView(View):
+    def get(self, request, *args, **kwargs):
+        notes = []
+        print("start Args")
+        print(args)
+        print("end Args")
+        if args:
+            notes = Note.objects.filter(folder=args[0])
+            print("Here")
+        else:
+            notes = Note.objects.filter(folder='d435ab9e-086d-4d1b-89d8-0843a8377a47')
+        folders = Folder.objects.all()
+        template = loader.get_template('homepage.html')
+        print(notes)
+        context = {
+            'folders': folders,
+            'notes': notes
+        }
+        return HttpResponse(template.render(context, request))
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST['id'])
+        print(kwargs)
+        return self.get(request, request.POST['id'], **kwargs)
+
+    
 
 
 
