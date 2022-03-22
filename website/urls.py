223ce8a8cffd4dfab1a919b68d5dad0444d795ad
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2022-03-22 05:45:09 -0600

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     # path('note/<id:pk>', NoteDetailView.as_view(), name='note_detail'),
     path('note/view/', ListNotesView.as_view(), name='list_notes'),
     path('note/<uuid:pk>/update/', NoteUpdateView.as_view(), name='update_note'),
-    #there's an error with fetching the url that I have yet to fix!
     path('note/<uuid:pk>/delete/', NoteDeleteView.as_view(), name='note_delete'),
     path('admin/', admin.site.urls),
 ]
